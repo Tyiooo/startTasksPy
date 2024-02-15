3fd5404a40a4a6a67cf2f857c7bc90f8b2bfbf24
Tyiooo <vailygione@gmail.com>
2024-02-15 20:18:52 +0300

    variables task done

---
+++ Basics/Exercise/2_variables/variables_solution.py
@@ -0,0 +1,14 @@
+#1
+print("break = 5")
+#2
+birth_year = 2004
+current_year = 2024
+age = current_year - birth_year
+print(age)
+#3
+first_name = "Egor"
+last_name = "Korortyuk"
+fathers_name = "Aleksndrovich"
+print(last_name + " " + first_name + " " + fathers_name)
+#4
+print("The answer is record^one, continue")
